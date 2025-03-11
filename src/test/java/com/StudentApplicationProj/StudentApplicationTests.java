9aec93c7ca4ecb3394e36670fdf0ad2df5a09eb0
Shubham Choudhary <shubham.choudhary@genzeon.com>
2025-03-11 13:40:19 +0530

    added springboot dependencies

---
+++ src/test/java/com/StudentApplicationProj/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentApplicationProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
